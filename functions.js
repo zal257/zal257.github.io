14f0aea533f9881c5839cc75de70a6cd6d3808a4
zal257 <zal257@email.vccs.edu>
2016-12-06 16:36:13 -0500

    Add files via upload
    Here we are, updated and nearly finished site. It was a while coming, but all the work was worth it. Let's see what others think!

--- functions.js
+++ functions.js
@@ -1,3 +1,160 @@
-function main() {
-	alert("Hello");
+var Merid = undefined;
+var hour1 = undefined;
+var minutes = undefined;
+
+function validate() {
+	var form = document.getElementById('pass');
+	if (form.value.length === 8) {
+		if(form.value === "password") {
+			alert("Welcome aboard!");
+			window.location.href = 'home.html';
+		}
+	}
+	else {
+		alert("Wrong password");
+	}
+}
+
+function checkTime() {
+	// This is where the clock gets its nutrients from.
+	var dayName = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+	var monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+	var now = new Date();
+	var day = now.getDay();
+	var month = now.getMonth();
+	var dayNum = now.getDate();
+	var year = now.getFullYear();
+	returnMerid();
+	returnHour1();
+	returnMinutes();
+	// And this spends all the energy the clock gets from its nutrients
+	var datentime = dayName[day] + ", " + monthName[month] + " " + dayNum + ", " + year + "<br/>" + hour1 + ":" + minutes + " " + Merid; 
+	document.getElementById("clock").innerHTML = datentime.toString();
+}
+
+function returnMerid() {
+	// For formatting purposes, don't mind this at all. Read the next function's comment to figure out what this does.
+	var now = new Date();
+	var hour = now.getHours();
+	
+	if (hour >= 13 & hour <= 24) {
+		Merid = "PM";
+	}
+	else{
+		Merid = "AM";
+	}
+}
+
+function returnHour1() {
+	// More formatting. Okay, this works with ReturnMerid to convert from Military to standard time.
+	var now = new Date();
+	var hour = now.getHours();
+	switch (hour) {
+		case 13 :
+			hour1 = 1;
+			break;
+		case 13 :
+			hour1 = 2;
+			break;
+		case 15 :
+			hour1 = 3;
+			break
+		case 16 :
+			hour1 = 4;
+			break;
+		case 17 :
+			hour1 = 5;
+			break;
+		case 18 :
+			hour1 = 6;
+			break;
+		case 19 :
+			hour1 = 7;
+			break;
+		case 20 :
+			hour1 = 8;
+			break;
+		case 21 :
+			hour1 = 9;
+			break;
+		case 22:
+			hour1 = 10;
+			break;
+		case 23:
+			hour1 = 11;
+			break;
+		case 24:
+			hour1 = 12;
+			break;
+		default:
+			hour1 = hour;
+	}
+}
+
+function returnMinutes() {
+	// Same as other functions for clock fomatting, except this works to add a zero to the beginning
+	// of single-digit minutes.
+	var now = new Date();
+	var minute = now.getMinutes();
+	switch (minute) {
+		case 0 :
+			minutes = '00';
+			break;
+		case 1 :
+			minutes = '01';
+			break;
+		case 2 :
+			minutes = '02';
+			break;
+		case 3 :
+			minutes = '03';
+			break;
+		case 4 :
+			minutes = '04';
+			break;
+		case 5 :
+			minutes = '05';
+			break;
+		case 6 :
+			minutes = '06';
+			break;
+		case 7 :
+			minutes = '07';
+			break;
+		case 8 :
+			minutes = '08';
+			break;
+		case 9 :
+			minutes = '09';
+			break;
+		default :
+			minutes = minute;
+	}
+}
+
+function audio() {
+	var audio = document.getElementById("audio");
+	if (hour1 > 7 && Merid == "PM") {
+		audio.play();
+		alert("Do as the frog says.");
+	}
+	else {
+		alert("You thought life was going to be this easy? Use the navigation bar at the top, ya bum!");
+	}
+}
+
+function timing() {
+	var time = document.getElementById('time');
+	var i =0;
+	while (i > 9, i++) {
+		time.innerHTML = i;
+	}
+	if (time.innerHTML == 9) {
+		setTimeout(timing1(time), 1000);
+	}
+}
+
+function timing1() {
+	var time = document.getElementById('time');
+	time.innerHTML = "There are ten things you need to know.";
 }
--- game.js
+++ game.js
@@ -5,10 +5,10 @@
 
 function fnLoad() {
 	// Here, the game's functions are loaded into the grid, and the option is given to the user
-	// to pick a board size of their choosing from an array of sizes, ranging from 3 x 3 to 100 x 100, arranged in squares
+	// to pick a board size of their choosing from an array of sizes, ranging from 3 x 3 to 10 x 10, arranged in squares
 	var select = document.getElementById("grid");
 	var tr = document.createElement('tr');
-	for (i = 3; i <= 100; i++) {
+	for (i = 3; i <= 10; i++) {
 		var option = document.createElement('option');
 		select.options[select.options.length] = new Option(i + ' X ' + i, i);
 	}
@@ -20,9 +20,8 @@ function fnLoad() {
 }
 
 function addEvent(element, eventName, callback) {
-	// This is where the event listener actually performs its function.
-	// If no event listener exists in the square that has been clicked on, then
-	// 
+	// From what I understand from all this, if no event listener exists in the square that has been clicked on, then
+	// one is created. This prevents squares from being overwritten in a current game.
 	if (element.addEventListener) {
 		element.addEventListener(eventName, callback, false);
 	} else if (element.attachEvent) {
@@ -31,9 +30,13 @@ function addEvent(element, eventName, callback) {
 }
 
 function fnChoose(e) {
+	// This function marks which squares had been filled. This determines if a full row of X's or O's have been completed,
+	// basically determines if the game has been won by either competitor, or drawn.
 	if (e.target && e.target.nodeName == "TD") {
 		var targetElement = document.getElementById(e.target.id);
 		var prevTurn;
+		// Here, boxes are checked for whether they can be filled or not.
+		// If a box hasn't been chosen, then it is filled with the marker of whosever turn it is.
 		if ((targetElement.className).indexOf("disabled") == -1) {
 			targetElement.innerHTML = turn;
 			targetElement.classList.add('disabled');
@@ -43,22 +46,29 @@ function fnChoose(e) {
 			turn = turn === "X" ? "O" : "X";
 			if (fndecide(targetElement, prevTurn)) {
 				alert(prevTurn + ' has won the game');
-				fnNewGame();
+				fnNewGame();	// Checks if X or O has won the game.
 			} else if ((score['X'] + score['O']) == (gridValue * gridValue)) {
 				alert('Draw!');
-				fnNewGame();
+				fnNewGame();	// If nobody has completed any rows in the entire board, then it is a draw.
 			}
 		}
 	}
 }
 
 function fndecide(targetElement, prevTurn) {
+	// This function determines whether the game has been won by either player.
 	var UL = document.getElementById('game');
 	var elements, i, j, cnt;
+	// Checks to see if the score of whoever went last is greater than the size of the board grid.
+	// If it is, then the count in this function, used to determine whether a row of X's or O's has been filled,
+	// is increased. If the counted number of markers (in a row) placed becomes equal to the size of the grid,
+	// Then the function returns a value of true to fnchoose, which then determines whether the game has ended
+	// in a tie, or if there has been a winner.
 	if (score[prevTurn] >= gridValue) {
 		var classes = targetElement.className.split(/\s+/);
 		for (i = 0; i < classes.length; i += 1) {
 			cnt = 0;
+			// Checks for matching row horizontally, vertically, and diagonally
 			if (classes[i].indexOf('row') !== -1 || classes[i].indexOf('col') !== -1 || classes[i].indexOf('dia') !== -1) {
 				elements = UL.getElementsByClassName(classes[i]);
 				for (j = 0; j < elements.length; j += 1) {
@@ -66,16 +76,21 @@ function fndecide(targetElement, prevTurn) {
 						cnt += 1;
 					}
 				}
+				// As mentioned previously, this returns true to fnchoose, supposing the row of markers counted
+				// matches the size of the grid.
 				if (cnt == gridValue) {
 					return true;
 				}
 			}
 		}
 	}
+	// Returns false by default to prevent the game from declaring a nonsensical victory or draw before the game even progresses
+	// far enough.
 	return false;
 }
 
 function fnNewGame() {
+	// This function starts a new game based on the board size picked by the user.
 	var gameUL = document.getElementById("game");
 	if (gameUL.innerHTML !== '') {
 		gameUL.innerHTML = null;
@@ -83,15 +98,16 @@ function fnNewGame() {
 			'X': 0,
 			'O': 0
 		};
+		// Defaults the first turn to X. Can be modified to make sure O goes first.
 		turn = 'X';
 		gridValue = 0;
 	}
+	// This selects the grid so that it can be modified
 	var select = document.getElementById("grid");
 	gridValue = select.options[select.selectedIndex].value;
-	var i, j, li, k = 0,
-		classLists;
+	var i, j, li, k = 0, classLists;	// Declares variables, sets their values and pairs them with classList DOMTokenList objects.
 	var gridAdd = +gridValue + 1;
-
+	// Grid size is generated according to the option chosen by the player.
 	for (i = 1; i <= gridValue; i += 1) {
 		tr = document.createElement('tr');
 		for (j = 1; j <= gridValue; j += 1) {
@@ -99,8 +115,8 @@ function fnNewGame() {
 			li = document.createElement('td');
 			li.setAttribute("id", 'li' + k);
 
-			classLists = 'td row' + i + ' col' + j;
-
+			classLists = 'td row' + i + ' col' + j;	// Adds 
+			
 			if (i === j) {
 				classLists = 'td row' + i + ' col' + j + ' dia0';
 			}
@@ -122,5 +138,6 @@ function fnNewGame() {
 
 
 function isEven(value) {
+	// Just checks for an even number. Probably used to determine if X or O goes for that turn.
 	return value % 2 == 0;
 }
